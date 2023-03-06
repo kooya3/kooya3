2f7c469a115c16ed6bdda753716a37089fa86d1a
kooya3 <elyeestatua@gmail.com>
2023-03-07 01:15:51 +0300

    importing components for the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 const App = ()  => {
 return (
     <BrowserROuter>
-      <div className='relative z-0 bg-primary '>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
@@ -15,7 +15,7 @@ return (
         <Tech />
         <Works />
         <Feedbacks />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React,  { useState, useEffect } from 'react';
+import { Link  } from 'react-router-dom';
+import { styles } from '../styles';
+import { NavLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   return (
