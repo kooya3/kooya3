74452c25d7d11ec57c1ddf9b47163fdf3ff03e93
kooya3 <elyeestatua@gmail.com>
2023-03-07 16:27:55 +0300

    created a higher order component folder to align everything centerly

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,15 +4,45 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='coding-lang-icons'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Overview</p>
-        <h2 className={styles.sectionHeadText}>About Me.</h2>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
@@ -20,8 +50,14 @@ const About = () => {
       I'm a skilled software developer from Kenya, proficient in TypeScript, JavaScript, React, Node.js, and Three.js. I collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
       I am dedicated to delivering high-quality work and take pride in my ability to bring complex ideas to life. Whether you need a new application built from scratch, or an existing system updated and improved, I have the skills and expertise to make it happen.
       </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
 
-export default About
+export default SectionWrapper(About, "about");
---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionWrapper = () => {
+  return (
+    <div>SectionWrapper</div>
+  )
+}
+
+export default SectionWrapper
