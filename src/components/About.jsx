85a316558cf5b6e3a2e3f1fe55cc8b48ca9d5f69
kooya3 <elyeestatua@gmail.com>
2023-03-07 16:07:41 +0300

    created the about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,24 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
+
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Overview</p>
+        <h2 className={styles.sectionHeadText}>About Me.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+      I'm a skilled software developer from Kenya, proficient in TypeScript, JavaScript, React, Node.js, and Three.js. I collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
+      I am dedicated to delivering high-quality work and take pride in my ability to bring complex ideas to life. Whether you need a new application built from scratch, or an existing system updated and improved, I have the skills and expertise to make it happen.
+      </motion.p>
+    </>
+  );
+};
 
 export default About
