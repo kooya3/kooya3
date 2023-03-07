fc426bc69a8490f1305f9bd5babd0bcec1e3bdb9
kooya3 <elyeestatua@gmail.com>
2023-03-07 15:30:42 +0300

    setting the mobile properties fn

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -23,11 +23,11 @@ const Computers = () => {
       <pointLight intensity={1} />
       <ambientLight intensity={0.1} />
       <primitive
-      object={computer.scene} 
-      scale={ 0.75 }
-      position={[0, -3.25, -1.5]}
-      rotation={[-0.01, -0.2, -0.1]}     
-      />
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+        />
      </mesh>
   );
 };
@@ -70,7 +70,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
