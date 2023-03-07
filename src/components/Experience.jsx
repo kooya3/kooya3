06fbfffe8a194a93f68b4169ce695d0bd73d8e67
kooya3 <elyeestatua@gmail.com>
2023-03-07 18:32:37 +0300

    creating the imports for workexp file

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
 
 const Experience = () => {
   return (
