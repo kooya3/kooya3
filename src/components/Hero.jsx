6113dd1ac98fac23053d5cc7ed53d874f659ae58
kooya3 <elyeestatua@gmail.com>
2023-03-07 15:12:41 +0300

    modifying the computer to fit on a mobile sized screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,7 @@ const Hero = () => {
       <ComputersCanvas />
       {/* creating the bouncing mouse using framer motion */}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
@@ -40,6 +41,7 @@ const Hero = () => {
               className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
       </div>
+      </a>
       </div>
     </section>
   )
