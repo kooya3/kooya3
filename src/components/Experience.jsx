86a1beb245a470c947ff21d53ba6b1ddfb85fa30
kooya3 <elyeestatua@gmail.com>
2023-03-07 18:44:44 +0300

    defined the content to render as well as style

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -32,8 +32,30 @@ const ExperienceCard = ({ experience }) => {
       </div>
     }
   >   
-  )
-  }
+<div>
+        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+        <p
+          className='text-secondary text-[16px] font-semibold'
+          style={{ margin: 0 }}
+        >
+          {experience.company_name}
+        </p>
+      </div>
+
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className='text-white-100 text-[14px] pl-1 tracking-wider'
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  );
+};
+  
 const Experience = () => {
   return (
     <>
