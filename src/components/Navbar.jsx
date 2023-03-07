05eb2b037d4805778a8c7fafabd8163980fb8cb6
kooya3 <elyeestatua@gmail.com>
2023-03-07 12:01:51 +0300

    created the toggle menu fn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,9 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav
     className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20`}
@@ -29,11 +31,21 @@ const Navbar = () => {
             <li
             key={link.id}
             className={`${active === link.title? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={() => setActive(link.title)}
             >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}        
         </ul>
+{/* mobile navigation bar */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img 
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain curosr-pointer" 
+          onClick={() => setToggle(!Toggle)}
+          />
+        </div>
       </div>
     </nav>
   )
