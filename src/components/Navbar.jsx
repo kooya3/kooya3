e96e7aeba41ab64b21b5069deee7065ff6411c1b
kooya3 <elyeestatua@gmail.com>
2023-03-07 09:13:47 +0300

    creating the name icon for the page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,24 @@ import { NavLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
   return (
-    <div>Navbar</div>
+    <nav
+    className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+        to="/"
+        className="flex items-center gap-2"
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+        }}
+        >
+          <img src={logo}  alt="logo" className="w-9 h-9 object-contain" />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
