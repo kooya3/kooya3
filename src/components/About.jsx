3d417a1eeac3e51040476df8fb473fc7b577bb59
kooya3 <elyeestatua@gmail.com>
2023-03-07 15:41:30 +0300

    importing services for the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 const About = () => {
   return (
     <div>About</div>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -37,7 +37,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
