b6ff635ef1b5bc92d42ccda4c586232f7a8f7d73
kooya3 <elyeestatua@gmail.com>
2023-03-07 14:26:05 +0300

    started editing the 3dimensional computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,9 +5,40 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
-    <div>Computers</div>
+     <mesh>
+      {/* creating a light to allow user to see */}
+      <hemisphereLight intensity={0.15}
+      groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+     </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+    frameLoop="demand"
+    shadows
+    camera={{ position: [20, 3, 5], fov: 25 }}  /* field of view  */  
+    gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* Adding a suspense from react to allow to show a loader as the model loads */}
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
