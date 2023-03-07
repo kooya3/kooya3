e811211ca2e018e0891f57c840a60e08b3905563
kooya3 <elyeestatua@gmail.com>
2023-03-07 22:31:23 +0300

    styling the ball component

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Ball = () => {
   return (
