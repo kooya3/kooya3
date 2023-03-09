1e12b65b065fbdb32518197e30f4ad94e53eb8b1
kooya3 <elyeestatua@gmail.com>
2023-03-09 11:58:37 +0300

    finished on the earth model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -81,7 +81,7 @@ const Contact = () => {
 
       <motion.div
       variants={slideIn('right', "tween", 0.2, 1)}
-      className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      className="xl:flex-1 xl:h-[630px] md:h-[450px] h-[350px]"
       >
         <EarthCanvas />
       </motion.div>
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -12,29 +12,33 @@ const Earth = () => {
   );
 };
 
-
 const EarthCanvas = () => {
   return (
     <Canvas
-    shadows
-    frameloop="demand"
-    gl={{ preserveDrawingBuffer: true }}
-    camera={{ }}
+      shadows
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
-        autoRotate
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
-
         <Earth />
+
+        <Preload all />
       </Suspense>
-
     </Canvas>
-  )
-}
-
+  );
+};
 
 export default EarthCanvas;
