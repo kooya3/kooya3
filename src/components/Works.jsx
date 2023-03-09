0c065647324fe029bca2b98eca8369de85504e9d
kooya3 <elyeestatua@gmail.com>
2023-03-09 10:20:29 +0300

    created necessary imports for the work component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
 
-const Works = () => {
+
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) 
+const gitWorks = () => {
   return (
     <div>Works</div>
   )
