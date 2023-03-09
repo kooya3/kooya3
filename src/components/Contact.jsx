d9bbe3153a29324547f40dc7e99f7266099a335c
kooya3 <elyeestatua@gmail.com>
2023-03-09 11:07:00 +0300

    started editing and creating contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+    });
+
+  const [loading, setLoading] = useState(false);
+  
+  const handleChange = () => {}
+
+  const handleSubmit = () => {}
+  
   return (
     <div>Contact</div>
   )
--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -14,6 +14,34 @@ const FeedbackCard = ({
   company,
   image,
 }) => (
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+  >
+    <p className='text-white font-black text-[48px]'>"</p>
+
+    <div className='mt-1'>
+      <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+      <p className='text-white font-black flex justify-end text-[48px]'>"</p>
+      <div className='mt-7 flex justify-between items-center gap-1'>
+        <div className='flex-1 flex flex-col'>
+          <p className='text-white font-medium text-[16px]'>
+            <span className='blue-text-gradient'>@</span> {name}
+          </p>
+          <p className='mt-1 text-secondary text-[12px]'>
+            {designation} of {company}
+          </p>
+        </div>
+
+        <img
+          src={image}
+          alt={`feedback_by-${name}`}
+          className='w-10 h-10 rounded-full object-cover'
+        />
+      </div>
+    </div>
+  </motion.div>
+);
 
 const Feedbacks = () => {
   return (
@@ -35,4 +63,4 @@ const Feedbacks = () => {
   );
 };
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "");
