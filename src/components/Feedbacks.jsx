422a6ab1192687e45decb1ce88b58e4299e98e1a
kooya3 <elyeestatua@gmail.com>
2023-03-09 10:50:41 +0300

    starting on the testimonial cards

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,10 +6,33 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
+const FeedbackCard = ({
+  index,
+  testimonial,
+  name,
+  designation,
+  company,
+  image,
+}) => (
+
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+      <div
+        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Feedbacks
